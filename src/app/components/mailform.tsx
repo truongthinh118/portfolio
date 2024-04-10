eaab412035694a823a35d2f67db1c861ebfd27e0
truongthinh118001 <truongthinh118001@gmail.com>
2024-04-11 01:44:18 +0700

    update mail

--- src/app/components/mailform.tsx
+++ src/app/components/mailform.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { useState, useMemo, useRef } from 'react';
+import { useState, useMemo } from 'react';
 import { Input, Textarea } from '@nextui-org/react';
 import { Modal, ModalContent, ModalBody, useDisclosure } from '@nextui-org/react';
-import emailjs from '@emailjs/browser';
+import { sendEmail } from '../action/actions';
 
 export default function MailForm() {
-    const form = useRef(null);
+    const [name, setName] = useState("");
 
     const [address, setAddress] = useState("");
 
@@ -19,35 +19,22 @@ export default function MailForm() {
         return validateEmail(address) ? false : true;
     }, [address]);
 
-
     const [isSuccess, setSuccess] = useState<boolean>(false);
 
-    const sendEmail = (e: any) => {
-        e.preventDefault();
-        if (form.current) {
-            emailjs
-                .sendForm('service_uqp42mn', 'template_cam0i4u', form.current, {
-                    publicKey: '2gh4dkYQw-bT5Z6s9',
-                })
-                .then(
-                    () => {
-                        setSuccess(true);
-                        onOpen();
-                    },
-                    (error) => {
-                        setSuccess(false);
-                        onOpen();
-                    },
-                );
-        }
-    };
-
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    const send = async (e: FormData) => {
+        const status = sendEmail(e);
+        setSuccess((await status).status)
+        onOpen()
+    }
 
     return (
         <>
             <div className='p-8 mx-auto w-2/4 bg-gradient-to-r from-blue-300 rounded-xl'>
-                <form ref={form} onSubmit={sendEmail} className="max-w-sm flex flex-col flex-wrap gap-4 text-[#5E6697]">
+                <form
+                    action={send}
+                    className="max-w-sm flex flex-col flex-wrap gap-4 text-[#5E6697]">
                     <Input type="name"
                         classNames={labelClass}
                         isRequired
@@ -58,6 +45,8 @@ export default function MailForm() {
                         startContent={
                             <i className="icon-form bi bi-person-circle" />
                         }
+                        value={name}
+                        onValueChange={setName}
                         name='name'
                         autoComplete='given-name'
 
@@ -74,6 +63,7 @@ export default function MailForm() {
                         }
                         name='email'
                         autoComplete='email'
+                        value={address}
                         onValueChange={setAddress}
                         errorMessage={isInvalid && "Please enter a valid email"}
                     />
@@ -85,16 +75,18 @@ export default function MailForm() {
                         placeholder="Enter your message"
                         isRequired
                     />
-                    <button
+                    <button type='submit'
                         className="text-white bg-blue-700
                                 hover:bg-blue-800 
                                 focus:ring-4 focus:outline-none focus:ring-blue-300 
                                 font-medium rounded-lg text-sm 
                                 w-full sm:w-auto px-5 py-2.5 
                                 text-center 
-                                dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                                dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >Send</button>
                 </form>
             </div>
+
             <Modal
                 backdrop='transparent'
                 isOpen={isOpen}
@@ -105,16 +97,16 @@ export default function MailForm() {
                 placement='bottom'
                 classNames={{
                     wrapper: "w-1/5	justify-start",
-                    base: "bg-green-300"
+                    closeButton: isSuccess?"hover:bg-green-400 active:bg-green-500":"hover:bg-red-400 active:bg-red-500"
                 }}
+                disableAnimation
             >
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <ModalBody className='flex-row gap-4'>
-                                
+                            <ModalBody className={`flex-row gap-4 px-3 ${isSuccess ? "bg-green-300":"bg-red-300"}`}>
                                 {isSuccess ?
-                                    (<> 
+                                    (<>
                                         <i className="bi bi-check-circle" />
                                         Message sent successfully.
                                     </>)
