7a4edac6751095f5d9abee4fb0b0e32e42427140
truongthinh118001 <truongthinh118001@gmail.com>
2024-05-15 00:45:02 +0700

    fix preloader

--- src/app/components/preloader.tsx
+++ src/app/components/preloader.tsx
@@ -1,43 +1,41 @@
-'use client';
+"use client";
 import { useState } from "react";
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Preloader() {
-    const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
 
-    function showContent() {
-        setIsVisible(false);
-        const contentElement = document.getElementById("app-container");
-        if (contentElement) {
-            contentElement.classList.remove("hidden");
-            contentElement.classList.add("flex");
-        }
+  function showContent() {
+    setIsVisible(false);
+    const contentElement = document.getElementById("app-container");
+    if (contentElement) {
+      contentElement.classList.remove("hidden");
+      // contentElement.classList.add("flex");
     }
+  }
 
-    return (
-        <AnimatePresence>
-            {isVisible && (
-                <motion.div
-                    className="preloader"
-                    key="preloader"
-                    initial={{ opacity: 1 }}
-                    animate={{ opacity: 0, transitionEnd: { display: "none" } }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 1, delay: 1.5 }}
-                    onAnimationComplete={() => showContent()}
-                >
-                    <motion.img
-                        src='logo.svg'
-                        className='h-3/4'
-                        initial={{ clipPath: 'inset(50% 0 50% 0)' }}
-                        animate={{ clipPath: 'inset(0)' }}
-                        transition={{ duration: 1.5 }}
-                        exit={{ clipPath: 'none' }}
-                    />
-                </motion.div>
-            )}
-        </AnimatePresence>
-
-    );
-};
-
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          className="preloader"
+          key="preloader"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0, transitionEnd: { display: "none" } }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1, delay: 1.5 }}
+          onAnimationComplete={() => showContent()}
+        >
+          <motion.img
+            src="logo.svg"
+            className="h-3/4"
+            initial={{ clipPath: "inset(50% 0 50% 0)" }}
+            animate={{ clipPath: "inset(0)" }}
+            transition={{ duration: 1.5 }}
+            exit={{ clipPath: "none" }}
+          />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import {useRouter} from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { NextUIProvider } from "@nextui-org/system";
 
-export function Providers({children}: { children: React.ReactNode }) {
-    const router = useRouter()
-    
-    return (
-      <NextUIProvider navigate={router.push}>
-        {children}
-      </NextUIProvider>
-    )
-  }
+export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <NextUIProvider
+      id="app-container"
+      navigate={router.push}
+      className="hidden"
+    >
+      {children}
+    </NextUIProvider>
+  );
+}
